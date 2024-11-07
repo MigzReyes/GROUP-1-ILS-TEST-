1a3ea1e2c07ca08925a0af6bbadbc20627538271
MigzReyes <johnmigzreyes08@example.com>
2024-11-07 22:23:26 +0800

    Updated Admin Page

---
+++ Group-1-ILS-Comprog-main/ILS CODE/HTML/Admin Page/layout/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../Admin Page/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="https://i.ibb.co/c3DkSHT/matsurika-10.png">
+  <title>
+    RAMEN Matsurika
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../Admin Page/assets/css/soft-ui-dashboard.css?v=1.1.0" rel="stylesheet" />
+  <!-- Nepcha Analytics (nepcha.com) -->
+  <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+
+    <?php include('layout/sidebar.php') ?>
+
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+
+            <?php include('layout/navbar.php') ?>
+
+                <div class="container-fluid py-4">
---
+++ Group-1-ILS-Comprog-main/ILS CODE/HTML/Admin Page/layout/sidebar.php
@@ -0,0 +1,59 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+<!--LOGO AND NAME-->
+    <div class="sidenav-header">
+      <a class="navbar-brand m-0" href="AdminPage.php">
+        <img src="https://i.ibb.co/c3DkSHT/matsurika-10.png" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold">RAMEN Matsurika</span>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+
+<!--MAIN SIDEBAR-->
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+
+        <!--DASHBOARD-->
+        <li class="nav-item">
+          <a class="nav-link  active" href="AdminPage.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="fa fa-home text-white text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+
+        <!--ENQUIRIES-->
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Enquiries</h6>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="../pages/tables.html">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="fa fa-bullhorn text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Enquiries</span>
+          </a>
+        </li>
+
+        <!--ADMIN / USER-->
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Site Management</h6>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="../pages/tables.html">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="fa fa-user text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Accounts</span>
+          </a>
+        </li>
+
+        
+
+<!--FOOTER-->
+    <div class="sidenav-footer mx-3 ">
+      <a class="btn btn-primary mt-3 w-100" href="../../RamenMatsurikaFrontPage.html">Back</a>
+    </div>
+</aside>
